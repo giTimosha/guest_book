5cd9e42f585374db16fd839e5204a21e4080c317
Admin <admin@MacBook-Air-Admin.local>
2019-09-21 13:36:13 +0600

    создал модель, добавил записи.

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from webapp.models import Record
+
+admin.site.register(Record)
 # Register your models here.
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+RECORD_STATUS_CHOICES = (
+    ('active', 'Активно'),
+    ('blocked', 'Заблокировано')
+)
+
+class Record(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Имя')
+    email = models.EmailField(verbose_name='Email')
+    text = models.TextField(max_length=2000, verbose_name='Запись')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    update = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+    status = models.CharField(max_length=20, verbose_name='Стратус',
+                                choices=RECORD_STATUS_CHOICES, default=RECORD_STATUS_CHOICES[0][0])
+
+    def __str__(self):
+        return self.name
